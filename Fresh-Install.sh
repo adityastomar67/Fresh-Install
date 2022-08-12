70e7a45bdf69d6dcfea0c61ba64fb43f84e19113
Aditya Singh Tomar <adityastomar12@gmail.com>
2022-08-12 07:15:07 +0530

    New Start

--- Fresh-Install.sh
+++ Fresh-Install.sh
@@ -7,6 +7,19 @@
 # Made for Arch Linux
 
 # TODO: Functionality to imply yes to all questions
-# TODO: Functionality to install packages from a file
+# TODO : Functionality to install packages from a file
 # TODO: Quiet mode
 # TODO: Verbose mode
+
+### HEADER
+gum style --foreground 212 --border-foreground 212 --border double --align center --width 50 --margin "1 2" --padding "2 4" 'Fresh Install' 'By - adityastomar67'
+
+### Checks
+
+## Check wether the go is installed or not
+command -v go >/dev/null 2>&1 || { echo >&2 "I require foo but it's not installed.  Aborting."; exit 1; }
+
+## Installing Charmbracelet/gum for beautiful UI
+if [[ ! $(command -v gum) ]]; then
+    go install github.com/charmbracelet/gum@latest
+fi
