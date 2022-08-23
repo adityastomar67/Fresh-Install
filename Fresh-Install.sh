8fc709b2460726b32e6ce9f506281b0bb06143ce
Aditya Singh Tomar <adityastomar12@gmail.com>
2022-08-23 09:52:28 +0530

    Added GRUB theme install function

--- Fresh-Install.sh
+++ Fresh-Install.sh
@@ -11,21 +11,35 @@
 # TODO: Quiet mode
 # TODO: Verbose mode
 
-
 ### Checks
 
 ## Check wether the go is installed or not
 if [[ ! $(command -v go) ]]; then
-    echo >&2 "Script require Go but it's not installed."
-    read -p "Continue to install Go or Abort? (Y/N): " confirm && [[ $confirm == [yY] || $confirm == [yY][eE][sS] ]] || exit 1
-    CMD=$(curl -s https://go.dev/doc/install | grep "rm -rf" | sed -E 's/(\$ |<[\/a-zA-Z =\"-]*>)//g')
-    sudo sh "$CMD"
+	echo >&2 "Script require Go but it's not installed."
+	read -p "Continue to install Go or Abort? (Y/N): " confirm && [[ $confirm == [yY] || $confirm == [yY][eE][sS] ]] || exit 1
+	CMD=$(curl -s https://go.dev/doc/install | grep "rm -rf" | sed -E 's/(\$ |<[\/a-zA-Z =\"-]*>)//g')
+	sudo sh "$CMD"
 fi
 
 ## Installing Charmbracelet/gum for beautiful UI
 if [[ ! $(command -v gum) ]]; then
-    go install github.com/charmbracelet/gum@latest
+	go install github.com/charmbracelet/gum@latest
 fi
 
 ### HEADER
 gum style --foreground 212 --border-foreground 212 --border double --align center --width 50 --margin "1 2" --padding "2 4" 'Fresh Install' 'By - adityastomar67'
+
+_GRUB_Install() {
+	git clone https://github.com/catppuccin/grub.git /tmp/repos && cd "/tmp/repos/"
+	sudo cp -r src/* /usr/share/grub/themes/
+	TYPE=$(gum choose "catppuccin-latte" "catppuccin-frappe" "catppuccin-macchiato" "Catppuccin-mocha")
+	THEME="GRUB_THEME='/usr/share/grub/themes/$TYPE-grub-theme/theme.txt'"
+	sed -r -i "s/GRUB_THEME=\"([/.-][a-z]\w+)*\"/$THEME/" /etc/default/grub
+	sudo grub-mkconfig -o /boot/grub/grub.cfg
+}
+
+
+
+################  HELP  ####################
+## For GRUB Theme
+HELP_GRUB="https://github.com/catppuccin/grub"
