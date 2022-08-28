307161a505689190b5f4b5e72334bb66ec495bf4
Aditya Singh Tomar <adityastomar12@gmail.com>
2022-08-28 10:16:22 +0530

    test update

--- new.sh
+++ new.sh
@@ -1,8 +1,28 @@
 #!/bin/bash
 
-while getopts 'lha:' OPTION; do
-  case "$OPTION" in
-    l)
+# while getopts 'lha:' OPTION; do
+#   case "$OPTION" in
+#     l)
+#       echo "linuxconfig"
+#       ;;
+#     h)
+#       echo "you have supplied the -h option"
+#       ;;
+#     a)
+#       avalue="$OPTARG"
+#       echo "The value provided is $OPTARG"
+#       ;;
+#     ?)
+#       echo "script usage: $(basename \$0) [-l] [-h] [-a somevalue]" >&2
+#       exit 1
+#       ;;
+#   esac
+# done
+# shift "$(($OPTIND -1))"
+
+if [ $# -gt 0 ]; then
+	case "$1" in
+    -l|--vim)
       echo "linuxconfig"
       ;;
     h)
@@ -16,6 +36,5 @@ while getopts 'lha:' OPTION; do
       echo "script usage: $(basename \$0) [-l] [-h] [-a somevalue]" >&2
       exit 1
       ;;
-  esac
-done
-shift "$(($OPTIND -1))"
+	esac
+fi
