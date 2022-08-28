9c271546cb576b3003e4c82209bbec56801b8b10
Aditya Singh Tomar <adityastomar12@gmail.com>
2022-08-28 23:03:24 +0530

    test

--- new.sh
+++ new.sh
@@ -22,21 +22,21 @@
 
 if [ $# -gt 0 ]; then
 	case "$1" in
-    -l|--vim)
-      echo "linuxconfig"
-      ;;
-    h)
-      echo "you have supplied the -h option"
-      ;;
-    a)
-      avalue="$OPTARG"
-      echo "The value provided is $OPTARG"
-      ;;
-    ?)
-      echo "script usage: $(basename \$0) [-l] [-h] [-a somevalue]" >&2
-      exit 1
-      ;;
+	-l | --vim)
+		echo "linuxconfig"
+		;;
+	h)
+		echo "you have supplied the -h option"
+		;;
+	a)
+		avalue="$OPTARG"
+		echo "The value provided is $OPTARG"
+		;;
+	?)
+		echo "script usage: $(basename \$0) [-l] [-h] [-a somevalue]" >&2
+		exit 1
+		;;
 	esac
 else
-    echo "No args"
+	echo "No args"
 fi
